1da84904b6bc62c63dc124a651012f4c21c6f58e
Ric <richardgregson1989@gmail.com>
2024-02-06 20:19:53 +0000

    abstract dateTime picker and others to outfit and clothing respectivley

--- screens/upcoming.tsx
+++ screens/upcoming.tsx
@@ -5,7 +5,7 @@ import { useClothingSelector } from '../store/hooks';
 import { DateTime } from 'luxon';
 import { ClothingItem } from '../store/slices/clothing-slice';
 import MonthCard from '../components/month-card';
-import { objectIsEmpty } from '../utils/helpers';
+import { groupByDate, objectIsEmpty } from '../utils/helpers';
 
 export default function UpcomingScreen() {
 	const allClothing = useClothingSelector(
@@ -19,21 +19,6 @@ export default function UpcomingScreen() {
 	filteredClothing.sort((a, b) => {
 		return DateTime.fromISO(a.date) - DateTime.fromISO(b.date);
 	});
-
-	const groupByDate = (filteredClothing: ClothingItem[]) => {
-		return filteredClothing.reduce((months, item) => {
-			if (!item.date) {
-				console.error('note.date is undefined');
-			} else {
-				const date = DateTime.fromISO(item.date).toFormat('MMMM');
-				if (!months.hasOwnProperty(date)) {
-					months[date] = [];
-				}
-				months[date].push(item);
-				return months;
-			}
-		}, {});
-	};
 
 	const grouped = groupByDate(filteredClothing);
 
--- utils/helpers.ts
+++ utils/helpers.ts
@@ -1,3 +1,6 @@
+import { ClothingItem } from '../store/slices/clothing-slice';
+import { DateTime } from 'luxon';
+
 export const uniqueId = () => {
 	const dateString = Date.now().toString(36);
 	const randomness = Math.random().toString(36).substr(2);
@@ -6,4 +9,19 @@ export const uniqueId = () => {
 
 export const objectIsEmpty = (obj: {}) => {
 	return Object.keys(obj).length === 0;
+};
+
+export const groupByDate = (filteredClothing: ClothingItem[]) => {
+	return filteredClothing.reduce((months, item) => {
+		if (!item.date) {
+			console.error('note.date is undefined');
+		} else {
+			const date = DateTime.fromISO(item.date).toFormat('MMMM');
+			if (!months.hasOwnProperty(date)) {
+				months[date] = [];
+			}
+			months[date].push(item);
+			return months;
+		}
+	}, {});
 };
